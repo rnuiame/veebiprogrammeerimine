e5b7a77ba7c4ac0a19ad2436f7c0f6d62a55812a
Revor Jan Nuiamäe <rnuiame@users.noreply.github.com>
2018-09-25 11:40:28 +0300

    Lisatud sõnumite kirjutamine ja lugemine

---
+++ Tund_4/addmsg.php
@@ -0,0 +1,37 @@
+<?php
+//kutsume välja funktisoonide faili
+require("functions.php");
+$notice = null;
+
+if (isset($_POST["submitMessage"])){
+	if ($_POST["message"] != "Siia sisesta oma sõnum..." and !empty($_POST["message"])){
+		$message = test_input($_POST["message"]);
+		$notice = "Sõnum olemas";
+	} else {
+		$notice = "Palun kirjuta sõnum!";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Anonüümse sõnumi lisamine</title>
+</head>
+<body>
+  <h1>Sõnumi lisamine</h1>
+  <p>See leht on loodud <a href="https://www.tlu.ee/" target="_blank">TLÜ<a> õppetöö raames, ei pruugi parim välja näha ning kindlasti ei sisalda tõsiseltvõetavat sisu.</p>
+  <hr>
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <label>Sõnum (max 256 märki):</label>
+    <textarea rows="4" cols="64" name="message">Siia sisesta oma sõnum...</textarea>
+    <input type="submit" name="submitMessage" value="Salvesta sõnum">
+  </form>
+  <hr>
+  <p><?php echo $notice; ?></p>
+  
+</body>
+</html>
